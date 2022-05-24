2be97c8c8ef199983ce456102caa5b9e4820d7ba
Majda <majda1bounif@gmail.com>
2022-05-24 19:03:50 +0300

    x

--- index.py
+++ index.py
@@ -1,20 +1,9 @@
 import os
-
 from flask import Flask, render_template
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 app = Flask("__name__")
-run_with_ngrok(app)
+#run_with_ngrok(app)
 
-def start_ngrok():
-    from pyngrok import ngrok
-
-    url = ngrok.connect(5000).public_url
-    print(' * Tunnel URL:', url)
-if app.config['START_NGROK']:
-    start_ngrok()
-class Config:
-    # ...
-    START_NGROK = os.environ.get('START_NGROK') is not None
 
 @app.route('/')
 def hello():
@@ -31,8 +20,19 @@ def location():
     return render_template("location.html")
 
 
+try:
+        from cryptography import x509
+        from cryptography.x509.oid import NameOID
+        from cryptography.hazmat.backends import default_backend
+        from cryptography.hazmat.primitives import hashes
+        from cryptography.hazmat.primitives.asymmetric import rsa
+except ImportError:
+        raise TypeError("Using ad-hoc certificates requires the cryptography library.")
 
-
-
+backend = default_backend()
+pkey = rsa.generate_private_key(
+        public_exponent=65537, key_size=2048, backend=backend
+    )
+#ssl_context="adhoc"
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', ssl_context="adhoc", debug=True)
